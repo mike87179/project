b43ac30fe956de782d3e59e6f8d04e722e1c4d80
r05943138 <r05943138@ntu.edu.tw>
2017-11-17 19:32:30 +0800

    Add files via upload

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+wget --no-check-certificate 'https://www.dropbox.com/s/daqfxxay8825dfr/model.zip?dl=0' -O model.zip
+unzip model.zip
+rm model.zip
+
 python3 hw3_test.py $1 $2 
+
