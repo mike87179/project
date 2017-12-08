8102e3bec77139a6b97dc5b0c09d953c9952358c
r05943138 <r05943138@ntu.edu.tw>
2017-12-08 19:36:45 +0800

    Add files via upload

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+
+
+wget --no-check-certificate 'https://www.dropbox.com/s/426gg2xjp4qpqsm/emb.zip?dl=0' -O emb.zip
+unzip emb.zip
+
+
+
 python3 hw4_test.py $1 $2 ./emb ./model.h5
